c033320b5e8ff6bcb019faca327cbef6c29af409
KarthickBoopathy <karthickboopathy94@gmail.com>
2024-06-19 07:48:52 +0530

    api-creation

--- src/main/java/com/sms/student_managment_system/Controller/StudentController.java
+++ src/main/java/com/sms/student_managment_system/Controller/StudentController.java
@@ -1,8 +1,15 @@
 package com.sms.student_managment_system.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+    @GetMapping("/health")
+    public String healthCheckup(){
+        return "Status : UP";
+    }
 
 }
